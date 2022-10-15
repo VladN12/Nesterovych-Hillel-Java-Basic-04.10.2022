cb785e8f7e641d0c9730e1c03e0693c4ccb478f3
VladN12 <114262950+VladN12@users.noreply.github.com>
2022-10-15 23:13:54 +0300

    init commit

---
+++ JavaRush.java
@@ -0,0 +1,25 @@
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+
+public class JavaRush {
+
+
+    public static void main(String[] args) {
+        String same = "ссылки на строки одинаковые";
+        String different = "ссылки на строки разные";
+
+
+
+
+        int a = 5;
+
+
+        System.out.println(a);
+
+
+
+    }
+}
