2363fedf5666ba78f30013353b14f52b7f30724e
VladN12 <114262950+VladN12@users.noreply.github.com>
2022-10-15 23:30:30 +0300

    init commit

---
+++ HomeTask2.java
@@ -0,0 +1,16 @@
+public class HomeTask2 {
+    public static void main(String[] args) {
+
+        //имя достопремечательности
+        String name = "Monument to Catherine II";
+        System.out.println(name);
+
+        //координаты достопремечательности
+        double x = 46.48733636058962;
+        double y = 30.739289178672394;
+        System.out.println(x);
+        System.out.println(y);
+
+
+    }
+}
