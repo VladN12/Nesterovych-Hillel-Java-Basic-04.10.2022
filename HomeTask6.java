15b2864196d263012c1023c7d7fe319984ebdbe3
VladN12 <114262950+VladN12@users.noreply.github.com>
2022-10-15 22:42:44 +0300

    Add files via upload

---
+++ HomeTask6.java
@@ -0,0 +1,38 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class HomeTask6 {
+
+    public static void main(String[] args) {
+
+
+        String team1 = "Terrorist";
+        int firstPlayerT = 15;
+        int secondPlayerT = 21;
+        int thirdPlayerT = 7;
+        int fourthPlayerT = 12;
+        int fifthPlayerT = 25;
+
+
+        double averageNumbersOfKillsT = (firstPlayerT + secondPlayerT + thirdPlayerT + fourthPlayerT + fifthPlayerT) / 5;
+
+
+        String team2 = "Counter-Terrorist";
+        int firstPlayerCT = 11;
+        int secondPlayerCT = 24;
+        int thirdPlayerCT = 12;
+        int fourthPlayerCT = 17;
+        int fifthPlayerCT = 8;
+
+        double averageNumbersOfKillsCT = (firstPlayerCT + secondPlayerCT + thirdPlayerCT + fourthPlayerCT + fifthPlayerCT) / 5;
+
+        if (averageNumbersOfKillsT > averageNumbersOfKillsCT)
+            System.out.println("Победила команда " + team1 + " набравшая " + averageNumbersOfKillsT + " очков");
+
+
+        else
+            System.out.println("Победила команда " + team2 + " набравшая " + averageNumbersOfKillsCT + " очков");
+
+
+    }
+
+}
