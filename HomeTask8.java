56cf63d7bbc00b92b8941d040405d4cb775dd97e
VladN12 <114262950+VladN12@users.noreply.github.com>
2022-10-22 00:09:15 +0300

    init commit

--- HomeTask8.java
+++ HomeTask8.java
@@ -9,45 +9,48 @@ public class HomeTask8 {
         int i = 0;
 
         System.out.println("Возраст Игроков Первой Команды ! ");
-
+        first:
         for (; i < team1.length; i++) {
 
             Random random = new Random();
             team1[i] = 18 + random.nextInt(40 - 18);
-            System.out.println(team1[i] + " " + " Лет ");
+            System.out.print(team1[i] + " " + " Лет " + " , ");
         }
 
         double sum1 = 0;
-        for (int x = 0 ;x<team1.length; x++) {
+        for (int x = 0; x < team1.length; x++) {
             sum1 += team1[x];
         }
+        System.out.println("");
         System.out.println("Средний возраст команды номер 1 = : " + sum1 / team1.length);
         System.out.println("---------------------------------------------");
 
 
         int[] team2;
         team2 = new int[25];
-        int x = 0;
+        int y = 0;
 
         System.out.println("Возраст Игроков Второй Команды ! ");
-
-        for (; x < team2.length; x++) {
+        second:
+        for (; y < team2.length; y++) {
 
             Random s = new Random();
-            team2[x] = 18 + s.nextInt(40 - 18);
-            System.out.println(team2[x] + " " + " Лет ");
+            team2[y] = 18 + s.nextInt(40 - 18);
+            System.out.print(team2[y] + " " + " Лет " + " , ");
 
 
         }
+
         double sum2 = 0;
-        for (int z=0; z<team2.length;z++) {
-            sum2 += team1[z];
+        for (int z = 0; z < team2.length; z++) {
+            sum2 += team2[z];
         }
-        System.out.println("Средний возраст команды номер 2 = : " + sum2 / team1.length);
+        System.out.println("");
+        System.out.println("Средний возраст команды номер 2 = : " + sum2 / team2.length);
         System.out.println("---------------------------------------------");
 
 
-        if (sum1 > sum2)
+       if (sum1 > sum2)
             System.out.println("Средний возраст первой команды больше");
 
         else if (sum1 == sum2)
